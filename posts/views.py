ae2f52d07d69d0863ad725426f37c31b1ba9ae79
verachecheneva <74712988+verachecheneva@users.noreply.github.com>
2021-01-28 23:02:29 +0300

    надо тестики сделать

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
 
 from .models import Post, Group
 from .forms import PostForm
-
-
-User = get_user_model()
 
 
 def index(request):
