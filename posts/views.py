8a031eadbf0676281d94492b8ae69d977f619a95
verachecheneva <74712988+verachecheneva@users.noreply.github.com>
2021-02-02 22:53:28 +0300

    no

--- posts/views.py
+++ posts/views.py
@@ -26,9 +26,6 @@ def group_posts(request, slug):
 
 @login_required
 def new_post(request):
-    title = "Добавить зпись"
-    header = "Добавить запись"
-    button = "Добавить"
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -36,9 +33,9 @@ def new_post(request):
             post.author = request.user
             post.save()
             return redirect("index")
-        return render(request, "new_post.html", {"form": form, "title": title, "header": header, "button": button})
+        return render(request, "new_post.html", {"form": form})
     form = PostForm()
-    return render(request, "new_post.html", {"form": form, "title": title, "header": header, "button": button})
+    return render(request, "new_post.html", {"form": form})
 
 
 User = get_user_model()
@@ -51,38 +48,33 @@ def profile(request, username):
     paginator = Paginator(posts_list, 10)
     page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
-    return render(request, "profile.html", {"username": username, "page": page, "author": author, "num": num})
+    return render(request, "profile.html", {"page": page, "author": author, "num": num})
 
 
 def post_view(request, username, post_id):
     author = get_object_or_404(User, username=username)
     posts_list = Post.objects.filter(username=username).order_by("-pub_date")
     num = posts_list.count()
-    text = Post.objacts.filter(username=username).filter(post_id=post_id)
-    return render(request, "post.html", {"username": username, "post_id": post_id, "num": num, "text": text,
+    post = Post.objacts.filter(username=username).filter(post_id=post_id)
+    return render(request, "post.html", {"post_id": post_id, "num": num, "post": post,
                                          "author": author})
 
 
 @login_required()
 def post_edit(request, username, post_id):
     if username != request.User.username:
-        author = get_object_or_404(User, username=username)
-        posts_list = Post.objects.filter(username=username).order_by("-pub_date")
-        num = posts_list.count()
-        text = Post.objacts.filter(username=username).filter(post_id=post_id)
-        return render(request, "post.html", {"username": username, "post_id": post_id, "num": num, "text": text,
-                                             "author": author})
+        return redirect("post_view", username, post_id)
     else:
-        title = "Редактировать зпись"
-        header = "Редактировать запись"
-        button = "Сохранить"
+        post = Post.objects.filter(username=username).filter(post_id=post_id)
         if request.method == "POST":
             form = PostForm(request.POST)
+            form.group = post.group
+            form.text = post.text
             if form.is_valid():
                 post = form.save(commit=False)
                 post.author = request.user
                 post.save()
                 return redirect("index")
-            return render(request, "new_post.html", {"form": form, "title": title, "header": header, "button": button})
+            return render(request, "new_post.html", {"form": form, "post": post})
         form = PostForm()
-        return render(request, "new_post.html", {"form": form, "title": title, "header": header, "button": button})
+        return render(request, "new_post.html", {"form": form, "post": post})
