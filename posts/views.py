d7c82ca8d0a022b43a419a087da00f708cb6bb78
verachecheneva <74712988+verachecheneva@users.noreply.github.com>
2021-02-05 16:11:55 +0300

    дз 6 с тестами исправленное

--- posts/views.py
+++ posts/views.py
@@ -44,19 +44,18 @@ User = get_user_model()
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts_list = Post.objects.filter(author=author).order_by("-pub_date")
-    num = posts_list.count()
     paginator = Paginator(posts_list, 10)
     page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
-    return render(request, "profile.html", {"page": page, "author": author, "num": num})
+    return render(request, "profile.html", {"page": page, "author": author})
 
 
 def post_view(request, username, post_id):
     author = get_object_or_404(User, username=username)
     posts_list = Post.objects.filter(author=author).order_by("-pub_date")
     num = posts_list.count()
-    post = Post.objects.filter(author=author).get(id=post_id)
-    return render(request, "post.html", {"post_id": post_id, "num": num, "post": post,
+    post = get_object_or_404(Post, author=author, id=post_id)
+    return render(request, "post.html", {"num": num, "post": post,
                                          "author": author})
 
 
@@ -66,13 +65,11 @@ def post_edit(request, username, post_id):
         return redirect(post_view, username, post_id)
     else:
         author = get_object_or_404(User, username=username)
-        post = Post.objects.filter(author=author).get(id=post_id)
+        post = get_object_or_404(Post, author=author, id=post_id)
         if request.method == "POST":
             form = PostForm(request.POST, instance=post)
             if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.save()
+                form.save()
                 return redirect(post_view, username, post_id)
             return render(request, "new_post.html", {"form": form, "post": post})
         form = PostForm(instance=post)
